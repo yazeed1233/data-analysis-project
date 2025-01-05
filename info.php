d51e23e7c765671a378453518230e9a843122fd0
yazeed <yazeedrashed04@gmail.com>
2025-01-05 21:05:14 +0000

    Updated project files

--- info.php
+++ info.php
@@ -1,3 +1,27 @@
 <?php
-phpinfo();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dictionary";
+
+// إنشاء اتصال
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// البحث عن الكلمة
+$word = $_GET['word'];
+$sql = "SELECT definition FROM words WHERE word='$word'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "Definition: " . $row["definition"];
+    }
+} else {
+    echo "Word not found";
+}
+$conn->close();
 ?>
