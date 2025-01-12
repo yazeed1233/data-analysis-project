5cf9c1f1d83354b9a46f57e4c8cf339f3e4d5ffc
yazeed <yazeedrashed04@gmail.com>
2025-01-12 22:46:57 +0000

    Deploying...

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 git add . && git commit -m "Deploying..." && git push origin main
-wget --user "yazeed_rashed_1" --password "118df85fc431b5187a15c0891967cc3972" "http://192.168.1.22:8080/job/job1/build?token=118df85fc431b5187a15c0891967cc3972"
+
+ curl -u "yazeed_rashed_1:118df85fc431b5187a15c0891967cc3972"\"http://192.168.1.22:8080/job/job1/build?token=118df85fc431b5187a15c0891967cc3972"
