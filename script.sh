f79f189af7bc618b20eca4e559a88e8c18b8151e
yazeed <yazeedrashed04@gmail.com>
2025-01-12 22:50:04 +0000

    Deploying...

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# إرسال التغييرات إلى GitHub
 git add . && git commit -m "Deploying..." && git push origin main
 
- curl -u "yazeed_rashed_1:118df85fc431b5187a15c0891967cc3972@http://192.168.1.22:8080/job/job1/build?token=118df85fc431b5187a15c0891967cc3972"
+# تشغيل الوظيفة في Jenkins باستخدام curl
+curl -u "yazeed_rashed_1:118df85fc431b5187a15c0891967cc3972" \
+     "http://192.168.1.22:8080/job/job1/build?token=118df85fc431b5187a15c0891967cc3972"
