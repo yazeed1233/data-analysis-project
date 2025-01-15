4cb5e00bb6b20b77f946598184a50713ed11e25e
yazeed <yazeedrashed04@gmail.com>
2025-01-15 17:04:09 +0000

    Deploying...

--- dumps/dump.sql
+++ dumps/dump.sql
@@ -1,35 +1,64 @@
--- حذف الجدول إذا كان موجودًا
+-- MySQL dump 10.13  Distrib 8.0.40, for Linux (x86_64)
+--
+-- Host: localhost    Database: dictionary
+-- ------------------------------------------------------
+-- Server version	8.0.40-0ubuntu0.24.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `months`
+--
+
 DROP TABLE IF EXISTS `months`;
-
--- إنشاء جدول جديد لتخزين أسماء الأشهر
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `months` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `gregorian_month` VARCHAR(50) NOT NULL, -- الاسم بالصيغة الجريجورية (الإنجليزية التقليدية)
-  `alternative_month` VARCHAR(50) NOT NULL, -- الاسم البديل بالإنجليزية
+  `id` int NOT NULL AUTO_INCREMENT,
+  `gregorian_month` varchar(50) DEFAULT NULL,
+  `alternative_month` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- إدخال البيانات الجديدة
-INSERT INTO `months` (`gregorian_month`, `alternative_month`) VALUES
-('January', 'Kanun Al-Thani'),
-('February', 'Shubat'),
-('March', 'Adar'),
-('April', 'Nisan'),
-('May', 'Ayyar'),
-('June', 'Haziran'),
-('July', 'Tammuz'),
-('August', 'Ab'),
-('September', 'Aylul'),
-('October', 'Tishrin Al-Awwal'),
-('November', 'Tishrin Al-Thani'),
-('December', 'Kanun Al-Awwal');
+--
+-- Dumping data for table `months`
+--
 
--- استعلام لاسترجاع الاسم البديل عند إدخال الاسم الجريجوري
-SELECT alternative_month
-FROM months
-WHERE gregorian_month = 'September';
+LOCK TABLES `months` WRITE;
+/*!40000 ALTER TABLE `months` DISABLE KEYS */;
+INSERT INTO `months` VALUES 
+(1, 'January', 'Kanun Al-Thani'),
+(2, 'February', 'Shubat'),
+(3, 'March', 'Adar'),
+(4, 'April', 'Nisan'),
+(5, 'May', 'Ayyar'),
+(6, 'June', 'Haziran'),
+(7, 'July', 'Tammuz'),
+(8, 'August', 'Ab'),
+(9, 'September', 'Aylul'),
+(10, 'October', 'Tishrin Al-Awwal'),
+(11, 'November', 'Tishrin Al-Thani'),
+(12, 'December', 'Kanun Al-Awwal');
+/*!40000 ALTER TABLE `months` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- استعلام لاسترجاع الاسم الجريجوري عند إدخال الاسم البديل
-SELECT gregorian_month
-FROM months
-WHERE alternative_month = 'Aylul';
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-01-15 19:23:53
