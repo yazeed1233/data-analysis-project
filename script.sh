a09ebc8eba33c0c9ac1a34097b8db4bc33a7af24
MahdyHamdan2001 <s11925614@stu.najah.edu>
2025-01-16 23:38:07 +0000

    Deploying...

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@
 git add . && git commit -m "Deploying..." && git push origin main
 
 curl -u "mahdyhamdan:11ea1478dfdd9bf3619851020b561ef330" 
-     "http://192.168.31.96:8380/job/job1/build?token=11ea1478dfdd9bf3619851020b561ef330"
+     "http://192.168.31.96:83:80/job/job1/build?token=11ea1478dfdd9bf3619851020b561ef330"
