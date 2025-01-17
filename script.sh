25cfb54f2c0e58d8a5e2f964fe0eae4e60076e06
MahdyHamdan2001 <s11925614@stu.najah.edu>
2025-01-17 14:16:45 +0000

    Token Comment

--- script.sh
+++ script.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 
-git add . && git commit -m "Deploying..." && git push origin main
+git add . && git commit -m "Token Comment" && git push origin main
 
-curl -u "yazeed_rashed_1:118df85fc431b5187a15c0891967cc3972" \
-     "http://192.168.1.22:8080/job/job1/build?token=118df85fc431b5187a15c0891967cc3972"
+curl -u "mahdyhamdan:118df85fc431b5187a15c0891967cc3972" \
+     "http://192.168.31.96:8080/job/job1/build?token=118df85fc431b5187a15c0891967cc3972"
+
+     #Yazeed Token : 118df85fc431b5187a15c0891967cc3972
+     #yazeed_rashed_1
+     #Yazeed IP : 192.168.1.22
+     #Mahdy Token : 11ea1478dfdd9bf3619851020b561ef330
+     #Mahdy IP : 192.168.31.96
+     #mahdyhamdan
