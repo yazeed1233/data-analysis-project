3d720ac541de9270b24c4e056ddcceefd22759eb
yazeed <yazeedrashed04@gmail.com>
2025-01-19 15:08:57 +0000

    Token Comment

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 git add . && git commit -m "Token Comment" && git push origin main
 
 curl -u "yazeed_rashed_1:118df85fc431b5187a15c0891967cc3972" \
-     "http://192.168.31.96:8080/job/job1/build?token=118df85fc431b5187a15c0891967cc3972"
+     "http://192.168.1.22:8080/job/job1/build?token=118df85fc431b5187a15c0891967cc3972"
 
      #Yazeed Token : 118df85fc431b5187a15c0891967cc3972
      #yazeed_rashed_1
